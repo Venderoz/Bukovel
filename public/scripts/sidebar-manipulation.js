474abe155a81586d5407d4bc5a37ac1d835b48ea
Venderoz <van.sob4625@gmail.com>
2024-02-07 14:31:07 +0200

    Upd: 05.02.2024
    - footer added and formatted
    - main colors changing
    - minor css modifying

--- public/scripts/sidebar-manipulation.js
+++ public/scripts/sidebar-manipulation.js
@@ -1,9 +1,11 @@
+const body = document.querySelector("body");
 const closeBtn = document.getElementById("close-btn");
 const showBtn = document.getElementById("show-sidebar-btn");
 
 closeBtn.addEventListener("click", () => {
     const sidebarEl = document.getElementById("sidebar-menu-container");
-
+    
+    body.style.overflowY = "visible";
     sidebarEl.classList.remove("sidebar-showing-animation");
     sidebarEl.classList.add("sidebar-hiding-animation");
     setTimeout(() => {sidebarEl.classList.remove("sidebar-enabled")}, 300);
@@ -11,6 +13,7 @@ closeBtn.addEventListener("click", () => {
 showBtn.addEventListener("click", () => {
     const sidebarEl = document.getElementById("sidebar-menu-container");
 
+    body.style.overflowY = "hidden";
     sidebarEl.classList.add("sidebar-showing-animation");
     sidebarEl.classList.remove("sidebar-hiding-animation");
     sidebarEl.classList.add("sidebar-enabled");
